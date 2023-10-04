b85d7a0b5886cd1266dcd1a8048b5f67d951f3d7
Håvard Brynjulfsen <havard.brynjulfsen@bouvet.no>
2023-10-04 16:08:58 +0200

    First commit for files in /studio and /web

---
+++ studio/sanity.config.ts
@@ -0,0 +1,19 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+
+export default defineConfig({
+  name: 'default',
+  title: 'Håvard Brynjulfsen',
+
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID || "",
+  dataset: process.env.SANITY_STUDIO_DATASET || "",
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
---
+++ studio/schemas/templates/richText.ts
@@ -0,0 +1,64 @@
+import { BiLinkAlt, BiLinkExternal } from "react-icons/bi";
+import { externalLinkUrl, internalLinkRef } from "./linkObject";
+
+export const richTextMain = {
+  title: 'Block Content',
+  name: 'blockContent',
+  type: 'array',
+  of: [
+    {
+      title: 'Block',
+      type: 'block',
+      // Styles let you set what your user can mark up blocks with. These
+      // correspond with HTML tags, but you can set any title or value
+      // you want and decide how you want to deal with it where you want to
+      // use your content.
+      styles: [
+        {title: 'Normal', value: 'normal'},
+        {title: 'H1', value: 'h1'},
+        {title: 'H2', value: 'h2'},
+        {title: 'H3', value: 'h3'},
+        {title: 'H4', value: 'h4'},
+        {title: 'Quote', value: 'blockquote'},
+      ],
+      lists: [{title: 'Bullet', value: 'bullet'}],
+      // Marks let you mark up inline text in the block editor.
+      marks: {
+        // Decorators usually describe a single property – e.g. a typographic
+        // preference or highlighting by editors.
+        decorators: [
+          {title: 'Strong', value: 'strong'},
+          {title: 'Emphasis', value: 'em'},
+        ],
+        // Annotations can be any object structure – e.g. a link or a footnote.
+        annotations: [
+          {
+            title: 'Internal Link',
+            name: 'internalLink',
+            icon: BiLinkAlt,
+            type: 'object',
+            fields: [
+              internalLinkRef
+            ],
+          },
+          {
+            title: 'External Link',
+            name: 'externalLink',
+            icon: BiLinkExternal,
+            type: 'object',
+            fields: [
+              externalLinkUrl
+            ],
+          },
+        ],
+      },
+    },
+    // You can add additional types here. Note that you can't use
+    // primitive types such as 'string' and 'number' in the same array
+    // as a block type.
+    {
+      type: 'image',
+      options: {hotspot: true},
+    },
+  ],
+}
