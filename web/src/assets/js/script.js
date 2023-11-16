1cb88dd7ab5dde0491a8d90ec9e81f157bb806f6
Håvard Brynjulfsen <havardob@hotmail.com>
2023-11-16 09:42:40 +0100

    Mobile menu + localstorage for dark-theme

--- web/src/assets/js/script.js
+++ web/src/assets/js/script.js
@@ -1,6 +1,38 @@
-const html = document.documentElement;
-const themeToggle = document.getElementById('themeToggle');
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const html = document.documentElement;
+    const themeToggle = document.getElementById('themeToggle');
+    const menuThemeToggle = document.getElementById('menuThemeToggle');
 
-themeToggle.addEventListener('click', function(){
-    html.classList.toggle('dark-theme');
+    themeToggle.addEventListener('click', function () {
+        toggleDarkTheme();
+    })
+
+    menuThemeToggle.addEventListener('click', function () {
+        toggleDarkTheme();
+    })
+    
+    function toggleDarkTheme() {
+        html.classList.toggle('dark-theme');
+        if (localStorage.getItem('theme') == "dark-theme") {
+            localStorage.removeItem('theme')
+        } else {
+            localStorage.setItem('theme', 'dark-theme')
+        }
+    }
+    
+    const menuToggle = document.getElementById('menuToggle');
+    const menuContainer = document.getElementById('menuContainer');
+    
+    menuToggle.addEventListener('click', function () {
+        menuToggle.classList.toggle('is-open');
+        menuContainer.classList.toggle('is-open');
+    })
+
+    function setStoredTheme() {
+        let storedTheme = localStorage.getItem("theme");
+        html.classList.add(storedTheme);
+    }
+
+    document.onload = setStoredTheme();
 })
