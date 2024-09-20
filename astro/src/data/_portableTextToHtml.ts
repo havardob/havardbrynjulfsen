28bd2679d38323ea38254becd755d04f704177c6
Håvard Brynjulfsen <havardob@hotmail.com>
2024-09-20 15:39:49 +0200

    added posts

---
+++ astro/src/data/_portableTextToHtml.ts
@@ -0,0 +1,72 @@
+import hljs from "highlight.js";
+
+export const portableTextToHtml = {
+    marks: {
+        externalLink: ({ children, value }) => {
+            return `<a href="${value.href}">${children}</a>`;
+        },
+        internalLink: ({ children, value }) => {
+            return `<a href="${value.href}">${children}</a>`;
+        },
+    },
+    types: {
+        horizontalRule: ({ value }) => {
+            return `
+                <hr style="${value.isDashed && 'dashed'}" />
+            `
+        },
+        codeBlock: ({ value }: any) => {
+            const highlightedCode = hljs.highlight(
+                `${value.code.code}`,
+                { language: value.code.language }
+            ).value
+
+
+            return `<div class="code-block ${value.size}">
+                <span class="code-block__lang">
+                    <span class="u-hidden">Language:</span>
+                    ${value.altTitle ? value.altTitle : value.code.language}
+                </span>
+                <div class="code-block__code">
+                    <pre>${highlightedCode}</pre> 
+                </div>
+            </div>`
+        },
+        imageBlock: ({ value }) => {
+            return `
+                <figure class="image-block | ${value.size}">
+                    <div class="image-block__frame"> 
+                        <img src="${value.imageFile}" alt="" loading="lazy" class="${value.hasBorder && "has-border"}" data-dialog-trigger />
+                        <dialog class="image-dialog">
+                            <figure>
+                                <img src="${value.imageFile}" alt="" />
+                                <button class="image-dialog__close">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24""><path fill="currentColor" d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
+                                </button>
+                                ${value.text || value.credits ? `
+                                    <figcaption>
+                                        ${!value.text ? "" : `<span>${value.text}</span>`}
+                                        ${!value.credits ? "" : `<span><span class="u-hidden">Credit:</span>${value.credits}</span>`}
+                                    </figcaption>
+                                ` : ''}
+                            </figure>
+                        </dialog>
+                    </div>
+                    ${value.text || value.credits ? `
+                        <figcaption class="image-block__caption">
+                            ${!value.text ? "" : `<span class="image-block__description">${value.text}</span>`}
+                            ${!value.credits ? "" : `<span class="image-block__source"><span class="u-hidden">Credit:</span>${value.credits}</span>`}
+                        </figcaption>
+                    ` : ''}
+                </figure>`
+        },
+        embedBlock: ({ value }) => {
+            return `
+                <div class="embed-block ${value.size}">
+                    ${value.code}
+                </div>
+            `
+        }
+    }
+};
+
---
+++ astro/src/data/article.ts
@@ -0,0 +1,73 @@
+import { client } from "./_sanityClient";
+import mql from "@microlink/mql";
+import { generateSlug, generateRichText, getReadingTime, formatDate } from "./_utils";
+
+export const getArticleData = async function (slug: any) {
+    const query = `*[_type == "article" && slug.current == "${slug}"][0] { 
+        _id,
+        title,
+        leading,
+        publishedDate,
+        publishedExternally,
+        tag ->, 
+        "featuredImage": featuredImage.asset->url, 
+        showAsBanner,
+        "tagSlug": tag -> slug.current, 
+        "tagTitle": tag -> title, 
+        "slug": slug.current, 
+        body[] {  
+            ..., 
+            _type == "imageBlock" => {
+                "imageFile": imageFile.asset->url,
+            }, 
+            markDefs[] {    
+                ..., 
+                _type == "internalLink" => {
+                    "href": internalDocument-> slug.current  
+                }
+            } 
+        }       
+    }`
+
+    const data = await client.fetch(query);
+    // Get fullSlug
+    const fullSlug = await generateSlug(data._id)
+    data.fullSlug = fullSlug.slug;
+
+    // Convert body text to html
+    if (data.body) {
+        data.body = await generateRichText(data.body);
+        data.readingTime = getReadingTime(data.body);
+    } else {
+        data.readingTime = 1;
+    }
+
+    // Get external data with MQL
+    if (data.publishedExternally) {
+        const externalSiteData = await mql(data.publishedExternally.href);
+        data.publishedExternally.image = externalSiteData.data.logo.url;
+    }
+
+    // Get a sortable date format and a formatted date
+    if (data.publishedDate) {
+        const dateForSorting = new Date(data.publishedDate);
+        const rssDate = new Date(data.publishedDate);
+        data.rssDate = rssDate;
+        data.dateForSorting = dateForSorting.getTime();
+        data.publishedDate = formatDate(data.publishedDate);
+    }
+
+    // Generate tag slug 
+    if (data.tag) {
+        const tag = await generateSlug(data.tag._id)
+        data.tagSlug = tag.slug;
+    }
+
+    let breadcrumbs = [];
+    const grandParent = await generateSlug('articleArchive');
+    breadcrumbs.push({ title: grandParent.title, slug: grandParent.slug });
+    breadcrumbs.push({ title: data.tag.title, slug: data.tagSlug });
+    data.breadcrumbs = breadcrumbs;
+
+    return data;
+} 
