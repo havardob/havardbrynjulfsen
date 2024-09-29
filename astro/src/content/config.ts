03c9b03be4ce13dd90f93ee2b9d16d4d674d572f
Håvard Brynjulfsen <havardob@hotmail.com>
2024-09-29 10:12:03 +0200

    replaces all Sanity API calls with Collection calls

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -1,7 +1,11 @@
-import { defineCollection } from 'astro:content';
+import {defineCollection} from 'astro:content';
 import {getArticleData} from "../data/article.ts";
 import {getCreationData} from "../data/creation.ts";
 import {getSiteSettingsData} from "../data/siteSettings.ts";
+import {getFrontPageData} from "../data/frontPage.ts";
+import {getArticleArchiveData} from "../data/articleArchive.ts";
+import {getCreationArchiveData} from "../data/creationArchive.ts";
+import {getTagPageData} from "../data/tag.ts";
 
 const siteSettingsCollection = defineCollection({
     loader: async () => {
@@ -10,6 +14,46 @@ const siteSettingsCollection = defineCollection({
             id: siteSettings._id,
             ...siteSettings
         }];
+    }
+});
+
+const frontPageCollection = defineCollection({
+    loader: async () => {
+        const frontPage = await getFrontPageData();
+        return [{
+            id: frontPage._id,
+            ...frontPage
+        }];
+    }
+});
+
+const articleArchiveCollection = defineCollection({
+    loader: async () => {
+        const articleArchive = await getArticleArchiveData();
+        return [{
+            id: articleArchive._id,
+            ...articleArchive
+        }];
+    }
+});
+
+const creationArchiveCollection = defineCollection({
+    loader: async () => {
+        const creationArchive = await getCreationArchiveData();
+        return [{
+            id: creationArchive._id,
+            ...creationArchive
+        }];
+    }
+});
+
+const tagPageCollection = defineCollection({
+    loader: async () => {
+        const response = await getTagPageData();
+        return response.map((tagPage: any) => ({
+            id: tagPage._id,
+            ...tagPage
+        }));
     }
 });
 
@@ -33,4 +77,12 @@ const creations = defineCollection({
     }
 });
 
-export const collections = { "siteSettings": siteSettingsCollection, "articles": articles, "creations": creations };
+export const collections = {
+    "siteSettings": siteSettingsCollection,
+    "frontPage": frontPageCollection,
+    "articleArchive": articleArchiveCollection,
+    "creationArchive": creationArchiveCollection,
+    "tags": tagPageCollection,
+    "articles": articles,
+    "creations": creations
+};
