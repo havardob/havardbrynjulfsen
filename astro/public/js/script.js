bf8fa658db32b7edbf0af6c67e4bea511056ce69
Håvard Brynjulfsen <havardob@hotmail.com>
2024-11-14 14:31:14 +0100

    escape key listener when dialog is open

--- astro/public/js/script.js
+++ astro/public/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     const html = document.documentElement;
     const themeToggle = document.getElementById('themeToggle');
     const menuThemeToggle = document.getElementById('menuThemeToggle');
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     menuThemeToggle.addEventListener('click', function () {
         toggleDarkTheme();
     })
-    
+
     function toggleDarkTheme() {
         html.classList.toggle('theme-inverted');
         if (localStorage.getItem('theme') === "theme-inverted") {
@@ -20,10 +20,10 @@ document.addEventListener("DOMContentLoaded", function () {
             localStorage.setItem('theme', 'theme-inverted')
         }
     }
-    
+
     const menuToggle = document.getElementById('menuToggle');
     const menuContainer = document.getElementById('menuContainer');
-    
+
     menuToggle.addEventListener('click', function () {
         menuToggle.classList.toggle('is-open');
         menuContainer.classList.toggle('is-open');
@@ -42,18 +42,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Open image dialog
     const imageDialogTriggers = document.querySelectorAll("[data-dialog-trigger]");
-    imageDialogTriggers.forEach(trigger => {
-        trigger.style.cursor = "pointer";
-        const dialog = trigger.nextElementSibling;
-        const closeButton = dialog.querySelector("button");
-        trigger.addEventListener("click", () => {
-            html.style.overflowY = "hidden";
-            dialog.showModal();
+
+    if (imageDialogTriggers.length > 0) {
+        imageDialogTriggers.forEach(trigger => {
+            trigger.style.cursor = "pointer";
+            const dialog = trigger.nextElementSibling;
+            const closeButton = dialog.querySelector("button");
+            trigger.addEventListener("click", () => {
+                html.style.overflowY = "hidden";
+                dialog.showModal();
+                closeDialogListener(dialog);
+            });
+
+            closeButton.addEventListener("click", () => {
+                html.style.overflowY = "initial";
+                dialog.close();
+            })
         });
-        
-        closeButton.addEventListener("click", () => {
+    }
+
+    const closeDialogListener = (dialogElement) => document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
             html.style.overflowY = "initial";
-            dialog.close();
-        })
-    });
+            dialogElement.close();
+        }
+    })
 })
