3b53414834df305529a7063362d1c94a4470b4a9
Håvard Brynjulfsen <havardob@hotmail.com>
2025-05-24 09:20:51 +0200

    adds sitemap

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,10 +1,13 @@
 import {defineConfig} from 'astro/config';
 import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.havardbrynjulfsen.design',
-    integrations: [
-        react()
-    ]
+  site: 'https://www.havardbrynjulfsen.design',
+  integrations: [
+    react(),
+    sitemap()
+  ]
 });
